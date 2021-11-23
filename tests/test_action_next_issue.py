fa0fbcdc9757c6072d74c2cf6ac0af2f17c76c8c
Markus Hihn <markus.hihn@dcso.de>
2021-11-23 13:25:12 +0100

    Adjust Tests

--- tests/test_action_next_issue.py
+++ tests/test_action_next_issue.py
@@ -18,6 +18,7 @@ class NextIssueActionTestCase(BaseActionTestCase):
         value = None
 
     def setUp(self):
+        self.maxDiff = None
         self.issues_graphql_response = json.loads(self.get_fixture_content("next_issue_api_response.json"))
         self.tdh_issue_graphql_response = json.loads(self.get_fixture_content("tdh_issue_api_response.json"))
         self.next_issue_plugin_response = json.loads(self.get_fixture_content("next_issue_plugin_response.json"))
